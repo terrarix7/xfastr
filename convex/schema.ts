86c58c45cfda0e1e58840ba717c0a919f09e4e1f
terrarix7 <terrarix7@gmail.com>
2025-07-04 08:05:20 +0530

    delete that shit

--- convex/schema.ts
+++ convex/schema.ts
@@ -5,8 +5,4 @@ import { v } from "convex/values";
 // You can delete this file (schema.ts) and the
 // app will continue to work.
 // The schema provides more precise TypeScript types.
-export default defineSchema({
-  numbers: defineTable({
-    value: v.number(),
-  }),
-});
+export default defineSchema({});
